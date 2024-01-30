eb36980418c6a5e36b649e815e6e40ed17179889
vishaljha1710 <romanvkj2001@gmail.com>
2024-01-30 20:38:55 +0530

    Doc formatting

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,44 +1,65 @@
-import { useEffect, useRef, useState } from 'react'
-import './CSS/Todo.css'
-import { Todoitems } from './Todoitems';
+import { useEffect, useRef, useState } from "react";
+import "./CSS/Todo.css";
+import { Todoitems } from "./Todoitems";
 
 export const Todo = () => {
-  const [count,setcount]=useState(0);
-  const [task,settask]=useState([])
-  const taskref=useRef(null)
-  const add=()=>{
-    setcount(count+1);
-    settask([...task,{no:count,text:taskref.current.value,display:""}]);
-    taskref.current.value="";
-    
-  }
-  useEffect(()=>{
+  const [count, setcount] = useState(0);
+  const [task, settask] = useState([]);
+  const taskref = useRef(null);
+  const add = () => {
+    setcount(count + 1);
+    settask([
+      ...task,
+      { no: count + 1, text: taskref.current.value, display: "" },
+    ]);
+    taskref.current.value = "";
+  };
+  useEffect(() => {
     settask(JSON.parse(localStorage.getItem("task")));
-   setcount(JSON.parse(localStorage.getItem("task_count")));
-  },[])
+    setcount(JSON.parse(localStorage.getItem("task_count")));
+  }, []);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     setTimeout(() => {
-    console.log(task);
-    localStorage.setItem("task",JSON.stringify(task));
-    localStorage.setItem("task_count",JSON.stringify(count))
+      console.log(task);
+      localStorage.setItem("task", JSON.stringify(task));
+      localStorage.setItem("task_count", JSON.stringify(count));
     }, 50);
-
-  },[task])
+  }, [task]);
 
   return (
-    <div className='todo'>
-      <div className='todo-header'>To-Do-List</div>
-      <div className='todo-add'>
-        <input ref={taskref} type='text' placeholder='Add task here...' className='todo-input'/>
-        <div onClick={()=>{add()}} className="todo-add-btn">Add</div>
+    <div className="todo">
+      <div className="todo-header">To-Do-List</div>
+      <div className="todo-add">
+        <input
+          ref={taskref}
+          type="text"
+          placeholder="Add task here..."
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn"
+        >
+          Add
+        </div>
       </div>
       <div className="todo-list">
-       {task.map((items,index)=>{
-        return <Todoitems key={index} setcount={setcount} settask={settask} no={items.no} text={items.text} display={items.display} ></Todoitems>
-       })}
-        </div>
+        {task.map((items, index) => {
+          return (
+            <Todoitems
+              key={index}
+              setcount={setcount}
+              settask={settask}
+              no={items.no}
+              text={items.text}
+              display={items.display}
+            ></Todoitems>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
